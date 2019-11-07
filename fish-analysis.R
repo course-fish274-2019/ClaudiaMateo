76acefd584de330d69431ed7c74d7934f1eae6d9
Claudia Penney <crpenney@Claudias-MacBook-Pro.local>
2019-11-07 10:24:53 -0800

    Added scatterplot, length v scalelength

--- fish-analysis.R
+++ fish-analysis.R
@@ -3,3 +3,7 @@ library(dplyr)
 #Create categorical size column
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+library(tidyverse)
+ggplot(fish_data) +
+  geom_point(mapping = aes(x = length, y = scalelength, color = lakeid))
