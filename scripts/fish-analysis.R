b2eca22cbfb3dcfd743d727a69c12d764ac80eb4
Claudia Penney <crpenney@Claudias-MacBook-Pro.local>
2019-11-08 11:00:42 -0800

    Updating

---
+++ scripts/fish-analysis.R
@@ -0,0 +1,11 @@
+fish_data <- read.csv("data/Gaeta_etal_CLC_data1.csv")
+library(dplyr)
+#Create categorical size column
+fish_data_cat = fish_data %>%
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+library(tidyverse)
+ggplot(fish_data) +
+  geom_point(mapping = aes(x = length, y = scalelength, color = lakeid))
+ggplot(fish_data_cat) +
+  geom_histogram(mapping = aes(x = scalelength, fill = length_cat))
